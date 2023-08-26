2319db03edf5103da94bcdd653927dd913e575a2
SmallGuyBool <91603405+SmallGuyBool@users.noreply.github.com>
2023-08-26 23:36:28 +1000

    Update visuals and start town engine

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ window.onbeforeunload = function () {
 }
 /*End document loading event handling */
 
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
 /* Handle title screen events */
 const startButton = document.getElementById('start-button');
 const titleScreen = document.getElementById('title-screen');
@@ -21,3 +23,50 @@ titleScreen.addEventListener('animationend', () => {
     titleScreen.style.display = 'none'; // Hide the element after the animation ends
 });
 /* End title screen event handling */
+
+/* Add onclick info hiding */
+function closeInfo(name) {
+    // Remove text and hide
+    const infoBox = document.getElementById(name);
+    infoBox.querySelector("p").innerHTML = "";
+    infoBox.style.display = "none";
+}
+/* End onclick info hiding */
+
+
+/* Add onclick handling to interactable buildings */
+const interactableBuildings = Array.from(document.getElementsByClassName("interactable"));
+
+interactableBuildings.forEach(building => {
+    building.addEventListener("click", async function(event) {
+        // Function to handle building information
+        
+        // Get info box corresponding to the town clicked on
+        const townNum = event.target.id[event.target.id.length - 1];
+        
+        const infoBox = document.getElementById("info" + townNum);
+        const info = document.getElementById("data" + townNum).innerText;
+    
+        // Check box isn't already open
+        if(infoBox.style.display == "block") {
+            closeInfo(infoBox.id);
+            return;
+        }
+
+        infoBox.style.display = "block";
+
+        // Animate fade in
+        infoBox.style.animationName = "fadeIn";
+        infoBox.style.animationDuration = "0.5s";
+        infoBox.style.animationFillMode = "forwards";
+        
+
+        // Animate typing effect
+        for(let i = 0; i < info.length; i++) {
+            await timer(100);
+            infoBox.querySelector("p").innerHTML += info[i];
+        }
+        
+    });
+});
+/* End building interaction */
